c3315747e0ac6e03863e4a86a832d26f41fb9d83
jess <jlfinlayson4@gmail.com>
2019-08-25 15:16:57 -0400

    updated server.js

---
+++ server.js
@@ -0,0 +1,32 @@
+// Require Express Package 
+var express = require("express");
+// Require Mongoose Package 
+var mongoose = require("mongoose");
+// Require Express-Handlebars Package 
+var exphbs = require("express-handlebars");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+// Express
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Routes
+var routes = require("./routes/routes");
+app.use(routes);
+
+// Mongoose
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+// Start Server
+app.listen(PORT, function() {
+    console.log("App running on port" + PORT + "!")
+});
