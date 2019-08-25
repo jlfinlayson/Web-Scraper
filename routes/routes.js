1ac631f56c813705971c30096434d89d3496e415
jess <jlfinlayson4@gmail.com>
2019-08-25 15:25:02 -0400

    psuedocoded routes needed

--- routes/routes.js
+++ routes/routes.js
@@ -4,4 +4,34 @@ var axios = require("axios");
 var cheerio = require("cheerio");
 var mongojs = require("mongojs");
 
+//Home
+router.get("/", function(req, res) {
+
+});
+
+//Data
+router.get("/all", function(req, res) {
+
+});
+
+//Scraping Website
+router.get("/scrape", function(req, res) {
+
+});
+
+//All scraped articles
+router.get("/articles", function(req, res) {
+
+});
+
+//Saved Articles
+router.get("/saved", function(req, res) {
+
+});
+
+//Clear Articles
+router.get("/clear", function(req, res) {
+
+});
+
 module.exports = router;
