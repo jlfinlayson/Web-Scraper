af8afd4818fcdec4b112b34ab43e59cdb42e72a0
jess <jlfinlayson4@gmail.com>
2019-08-25 15:36:11 -0400

    updated scrape, articless, and home routers

--- routes/routes.js
+++ routes/routes.js
@@ -6,22 +6,73 @@ var mongojs = require("mongojs");
 
 //Home
 router.get("/", function(req, res) {
-
-});
-
-//Data
-router.get("/all", function(req, res) {
-
+    db.Article.find({})
+    .then(function(dbArticle) {
+      var hbsObject = {
+          articles: dbArticle
+      };
+      res.render("index", hbsObject);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
 });
 
 //Scraping Website
 router.get("/scrape", function(req, res) {
+    axios.get("https://www.altpress.com/news/").then(function(response) {
 
+    var $ = cheerio.load(response.data);
+
+    $(".td_module_10").each(function(i, element) {
+
+      var result = {};
+
+      result.title = $(this)
+        .children(".item-details")
+        .children("h3")
+        .text();
+
+      result.link = $(this)
+        .children(".item-details")
+        .children("h3")
+        .children("a")
+        .attr("href");
+
+      result.summary = $(this)
+        .children(".item-details")
+        .children(".td-excerpt")
+        .text();
+
+      result.image = $(this)
+        .children(".td-module-thumb")
+        .children("a")
+        .children("img")
+        .attr("src");
+
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    });
+  });
 });
 
 //All scraped articles
 router.get("/articles", function(req, res) {
-
+    db.Article.find({})
+    .then(function(dbArticle) {
+      var hbsObject = {
+          articles: dbArticle
+      };
+      res.render("index", hbsObject);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
 });
 
 //Saved Articles
