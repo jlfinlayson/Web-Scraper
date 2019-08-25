f261153c48ae785233d2c40c2f9169ba4e826f02
jess <jlfinlayson4@gmail.com>
2019-08-25 15:20:46 -0400

    routes.js packages required and exported

---
+++ routes/routes.js
@@ -0,0 +1,7 @@
+var router = require("express").Router();
+var db = require("../models");
+var axios = require("axios");
+var cheerio = require("cheerio");
+var mongojs = require("mongojs");
+
+module.exports = router;
